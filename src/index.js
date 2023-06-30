3f9b9ea4dcc856478dd9c54a11da5337ef8684ed
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-06-30 16:33:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,44 +3,87 @@ import Notiflix from 'notiflix'
 import SimpleLightbox from "simplelightbox"
 
 const gallery = document.querySelector('.gallery');
-const API_KEY = '37953562-c7c28b8f0c02ebea23bfb706a';
-const searchText = document.querySelector('.search-form input');
-const submit = document.querySelector('.submit')
+const searchText = document.querySelector('#search-form');
+const btnLoadMore = document.querySelector('.load-more');
 
-submit.addEventListener('click', searchPhotos)
+searchText.addEventListener('submit', searchPhotos)
+btnLoadMore.addEventListener('click', loadMore);
 
-function searchPhotos() {
-submit.preventDefault()
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: '250',
+});
+
+let page = 1;
+let querySearch;
+const perPage = 40;
+
+const toFetchImages = async (querySearch, page, perPage) => {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const params = new URLSearchParams({
+    key: '37953562-c7c28b8f0c02ebea23bfb706a',
+    q: querySearch,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: page,
+    per_page: perPage,
+  });
+  const response = await axios.get(`${BASE_URL}?${params.toString()}`);
+  return response;
+};
+
+function searchPhotos(find) {
+  find.preventDefault();
+  querySearch = find.currentTarget.searchQuery.value.trim();
+  gallery.innerHTML = '';
+  page = 1;
+  btnLoadMore.classList.add('hidden');
   gallery.style.display = 'none';
-  const findingText = searchText.value.toLowerCase();
-  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${findingText}&image_type=photo&orientation=horizontal&safesearch=true`;
+  render();
 
-  return fetch(URL)
-    .then((resp) => {
-      if (!resp.ok) throw new Error(resp.status);
-      return resp.json();
-    })
-    .then((data) => {
-      gallery.style.display = 'block';
-      showInfo(data.hits[0]);
-    })
-    .catch((error) => console.log(error));
+  async function render() {
+    try {
+      const { data: response } = await toFetchImages(querySearch, page, perPage);
+      if (querySearch === '') {
+        return Notiflix.Notify.failure(
+          "Sorry, there are no images matching your search query. Please try again."
+        );
+      }
+      if (response.totalHits === 0) {
+        return Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      } else {
+        response.hits.forEach((photo) => {
+          showInfo(photo);
+        });
+        Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
+        lightbox.refresh();
+        if (response.totalHits > perPage) {
+          btnLoadMore.classList.remove('hidden');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 function showInfo(photo) {
   const markup = `
-    <div class="photo-card">
-      <img src="${photo.userImageURL}" alt="${photo.tags}" loading="lazy" />
-      <div class="info">
-        <p class="info-item"><b>Likes: ${photo.likes}</b></p>
-        <p class="info-item"><b>Views: ${photo.views}</b></p>
-        <p class="info-item"><b>Comments: ${photo.comments}</b></p>
-        <p class="info-item"><b>Downloads: ${photo.downloads}</b></p>
+    <a class="gallery__link" href="${photo.largeImageURL}">
+      <div class="photo-card">
+        <img src="${photo.userImageURL}" alt="${photo.tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item"><b>Likes: ${photo.likes}</b></p>
+          <p class="info-item"><b>Views: ${photo.views}</b></p>
+          <p class="info-item"><b>Comments: ${photo.comments}</b></p>
+          <p class="info-item"><b>Downloads: ${photo.downloads}</b></p>
+        </div>
       </div>
-    </div>`;
+    </a>`;
 
-  gallery.innerHTML = markup;
-  const lightbox  =  $(`.photo-card img`).SimpleLightbox()
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
-
-searchText.addEventListener('change', searchPhotos);
