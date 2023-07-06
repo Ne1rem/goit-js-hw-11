cedccbe439229043ba0f2394ed9d6955dc2f09b3
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-07-06 18:50:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import axios from 'axios'
 import Notiflix from 'notiflix'
 import SimpleLightbox from "simplelightbox"
-import { toFetchImages } from './JS/toFetchImages';                    
-import { showInfo } from './JS/showInfo'
-
 const gallery = document.querySelector('.gallery');
 const searchText = document.querySelector('#search-form');
 const btnLoadMore = document.querySelector('.load-more');
-
 searchText.addEventListener('submit', searchPhotos)
 btnLoadMore.addEventListener('click', loadMore);
 const lightbox = new SimpleLightbox('.gallery a', {
@@ -16,11 +12,24 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: '250',
 });
 
-const lightbox = new SimpleLightbox('.gallery a')
-btnLoadMore.style.display = 'none'
 let page = 1;
 let querySearch;
 const perPage = 40;
+
+const toFetchImages = async (querySearch, page, perPage) => {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const params = new URLSearchParams({
+    key: '37953562-c7c28b8f0c02ebea23bfb706a',
+    q: querySearch,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: page,
+    per_page: perPage,
+  });
+  const response = await axios.get(`${BASE_URL}?${params.toString()}`);
+  return response;
+};
 
 function searchPhotos(find) {
   find.preventDefault();
@@ -28,9 +37,6 @@ function searchPhotos(find) {
   gallery.innerHTML = '';
   page = 1;
 
-
-  gallery.style.display = 'none';
-  
   gallery.style.display = 'none';
   render();
 
@@ -54,7 +60,7 @@ function searchPhotos(find) {
         Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
         lightbox.refresh();
         if (response.totalHits > perPage) {
-          btnLoadMore.style.display = 'block'
+          btnLoadMore.classList.remove('hidden');
         }
       }
     } catch (error) {
@@ -62,6 +68,33 @@ function searchPhotos(find) {
     }
   }
 }
+
+function showInfo(images) {
+  const galleryMarkup = images.map(
+      ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+      }) => {
+          return `<a class="gallery__link" href="${largeImageURL}">
+          <div class="photo-card">
+              <img src="${webformatURL}" alt="${tags}" loading="lazy" height="100" />
+              <div class="info">
+              <p class="info-item"><b>Likes</b> ${likes}</p> 
+              <p class="info-item"><b>Views</b> ${views}</p>
+              <p class="info-item"><b>Comments</b> ${comments}</p>
+              <p class="info-item"><b>Downloads</b> ${downloads}</p>
+            </div>
+          </div>;
+          </a>`;
+      }).join('');
+  gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+}    
 
 async function loadMore(){
   page +=1
@@ -71,7 +104,7 @@ async function loadMore(){
     lightbox.refresh();
     const totalPages = Math.ceil(response.totalHits / perPage)
     if (page === totalPages) {
-      btnLoadMore.style.display = 'none'
+        btnLoadMore.classList.add('hidden');
         Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
     }
 }
